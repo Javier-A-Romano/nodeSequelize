0fea39aa506bd38811da3bbc74947aaf617f43d7
Javier Romano <romanojavier92@gmail.com>
2022-11-11 10:32:26 -0300

    get for Person ID

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -29,13 +29,14 @@ export const createPerson = async (req, res) => {
 }
 export const updatePerson = async (req, res) => {
     try {
-        const { name } = req.body
+        const { id } = req.params;
+        const {name} =req.body;
 
-        const newPerson = await Person.create({
-            name
-        })
+        const personUddate = await Person.findByPk(id)
+        personUddate.name=name
+        await personUddate.save()
 
-        res.json(newPerson)
+        res.json(personUddate)
     } catch (error) {
         return res.status(500).json({
             message: error.message
@@ -54,6 +55,27 @@ export const deletePerson = async (req, res) => {
         })
 
         res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const getPersonID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+
+        const personID = await Person.findOne({
+            where:{
+                id:id
+            }
+        })
+        
+        if(!personID)return res.status(404).json({message : `person with ID ${id} does not exists `})
+
+        res.json(personID)
     } catch (error) {
         return res.status(500).json({
             message: error.message
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { getPerson,createPerson, updatePerson, deletePerson } from '../controllers/post.controller.js'
+import { getPerson,createPerson, updatePerson, deletePerson ,getPersonID} from '../controllers/post.controller.js'
 
 
 const router = Router()
 
 router.get('/person',getPerson)
+router.get('/person/:id',getPersonID)
 router.post('/person',createPerson)
 router.put('/person/:id',updatePerson)
 router.delete('/person/:id',deletePerson)
