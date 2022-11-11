f02d5e929b1e11a07098fede741349229d272dc7
Javier Romano <romanojavier92@gmail.com>
2022-11-11 10:11:59 -0300

    delete controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,23 +1,60 @@
-import {Person} from '../models/Person.js'
+import { Person } from '../models/Person.js'
 
-export const getPerson = async (req,res)=>{
-    try{const allPerson= await Person.findAll()
+export const getPerson = async (req, res) => {
+    try {
+        const allPerson = await Person.findAll()
 
-    res.json(allPerson)}catch(error){
+        res.json(allPerson)
+    } catch (error) {
         return res.status(500).json({
             message: error.message
         })
     }
-    
-}
-export const createPerson = async (req,res)=>{
-    try{const {name } = req.body
 
-    const newPerson = await Person.create({
-        name
-    })
-    
-    res.json(newPerson)}catch(error){
+}
+export const createPerson = async (req, res) => {
+    try {
+        const { name } = req.body
+
+        const newPerson = await Person.create({
+            name
+        })
+
+        res.json(newPerson)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+export const updatePerson = async (req, res) => {
+    try {
+        const { name } = req.body
+
+        const newPerson = await Person.create({
+            name
+        })
+
+        res.json(newPerson)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const deletePerson = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        await Person.destroy({
+            where: {
+                id,
+            }
+        })
+
+        res.sendStatus(204)
+    } catch (error) {
         return res.status(500).json({
             message: error.message
         })
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
-import { getPerson,createPerson } from '../controllers/post.controller.js'
+import { getPerson,createPerson, updatePerson, deletePerson } from '../controllers/post.controller.js'
 
 
 const router = Router()
 
 router.get('/person',getPerson)
 router.post('/person',createPerson)
+router.put('/person/:id',updatePerson)
+router.delete('/person/:id',deletePerson)
 
 
 export default router
