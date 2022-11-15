726ead2832eab269b9c537f008398182d14392e1
Javier Romano <romanojavier92@gmail.com>
2022-11-15 10:31:49 -0300

    add cookie with token

--- src/app.js
+++ src/app.js
@@ -2,19 +2,36 @@ import express from 'express'
 import personRoutes from './routes/person.routes.js'
 import postRoutes from './routes/post.routes.js'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
-app.post('/person/login', (req, res) => {
+app.get('/person/login', (req, res) => {
     const user = { id: 3 }
     const token = jwt.sign({ user }, 'secret_key')
-
+    res.cookie('token', token, {
+        maxAge: 10000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax'
+    })
     res.json({ token })
 })
-app.use(({ query }, res, next) => {
-    jwt.verify(query.token, 'secret_key', (err, data) => {
-        if (err) {res.sendStatus(403) } else { next()}
+app.get('/person/logout', (req, res) => {
+    if (req.cookies.token) {
+        res.clearCookie("token")
+        res.send("close session and delete cookie token")
+    } else {
+        res.send("you dont have token")
+    }
+
+})
+app.use(({ query, cookies }, res, next) => {
+    jwt.verify(cookies.token, 'secret_key', (err, data) => {
+
+        if (err) { res.sendStatus(403) } else { next() }
     })
 })
 app.use(personRoutes)
