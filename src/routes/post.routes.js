f724842210468cd423ba8b0666bf375597c75798
Javier Romano <romanojavier92@gmail.com>
2022-11-15 22:03:15 -0300

    add swagger for api POST

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -38,13 +38,13 @@ const router = Router()
  *        msg: Post was not found
  *
  *  parameters:
- *    PostId:
+ *    Idpost:
  *      in: path
  *      name: id
  *      required: true
  *      schema:
  *        type: string
- *      description: the task id
+ *      description: the post id
  */
 
 /**
@@ -59,7 +59,7 @@ const router = Router()
  * /post:
  *  get:
  *    summary: Returns a list of post
- *    tags: [post]
+ *    tags: [Post]
  *    responses:
  *      200:
  *        description: the list of post
@@ -70,10 +70,98 @@ const router = Router()
  *              items:
  *                $ref: '#/components/schemas/Post'
  */
+
 router.get('/post', getPost)
+
+/**
+ * @swagger
+ * /post:
+ *  post:
+ *    summary: create a new post
+ *    tags: [Post]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Post'
+ *    responses:
+ *      200:
+ *        description: the post was successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Post'
+ *      500:
+ *        description: Some server error
+ *
+ */
 router.post('/post', createPost)
+/**
+ * @swagger
+ * /post/{id}:
+ *  get:
+ *    summary: create a new post
+ *    tags: [Post]
+ *    parameters:
+ *      - $ref: '#/components/parameters/Idpost'
+ *    responses:
+ *      200:
+ *        description: the post was successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Post'
+ *      404:
+ *        description: Some server error
+ *
+ */
 router.get('/post/:id', getPostID)
+/**
+ * @swagger
+ * /post/{id}:
+ *  delete:
+ *    summary: delete a post
+ *    tags: [Post]
+ *    parameters:
+ *      - $ref: '#/components/parameters/Idpost'
+ *    responses:
+ *      204:
+ *        description: the post was deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Post'
+ *      404:
+ *        description: Some server error
+ *
+ */
 router.delete('/post/:id', deletePostID)
+/**
+ * @swagger
+ * /post/{id}:
+ *  put:
+ *    summary: update a post by id
+ *    tags: [Post]
+ *    parameters:
+ *      - $ref: '#/components/parameters/Idpost'
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Post'
+ *    responses:
+ *      200:
+ *        description: the post was updated 
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Post'
+ *      404:
+ *        description: Some server error
+ *
+ */
 router.put('/post/:id', updatePost)
 
 
