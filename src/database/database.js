113bd83127a297002b28c6c311d45c65d830c213
Javier Romano <romanojavier92@gmail.com>
2022-11-17 00:42:18 -0300

    deploy add DB URI

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,21 @@
 import Sequelize from 'sequelize'
+const dotenv = require('dotenv');
 
-export const sequelize = new Sequelize('projectsdb', 'postgres', 'shorlak', {
+/*export const sequelize = new Sequelize('projectsdb', 'postgres', 'shorlak', {
     host: 'localhost', dialect: 'postgres'
-})
+})*/
+
+dotenv.config();
+
+const {
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_URI
+} = process.env
+
+const sequelize = new Sequelize(DB_URI, {
+    define: {
+      timestamps: false
+    }
+  });
